6168ba3b4b235e68790064c7a249f49b64eca98b
SAMEER SHRESTHA <Nero0293@outlook.com>
2022-06-28 22:50:45 +0545

    I don't know what i'm doing but i'm trying

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const blackButton = document.getElementById("blackButton");
 const radnomButton = document.getElementById("randomButton");
 const eraseButton = document.getElementById("eraseButton");
 const resetButton = document.getElementById("resetButton");
+const greyButton = document.getElementById("greyButton");
+let btnContainer = document.getElementsByClassName("buttons");
 
-
+function clearGrid()
 {
-    console.log("black");
+    clearGrid();
+    renderSquare();
 }
+
 function renderSquare()
 {
         let board = document.querySelector(".board");
@@ -17,10 +21,25 @@ function renderSquare()
         for(let i = 0; i<256 ; i++)
         {
             let square = document.createElement("div");
-            square.style.backgroundColor = "#111161";
+            square.style.backgroundColor = "white";
             board.insertAdjacentElement("beforeend",square);
-
+            board.appendChild("div").add.classList('square');
         }
     }
 }
 renderSquare();
+
+function greyColor()
+{
+    const boxs = document.querySelectorAll('.square');
+    greyButton.textContent = "Grey";
+    greyButton.addEventListener("click",() =>
+    {
+        boxs.forEach(square => square.addEventListener("mouseover", () =>{
+            let Rnum = Math.floor(Math.random()*255)
+            square.style.backgroundColor = `rgb($(Rnum),$(Rnum),$(Rnum)) `;
+        }));
+    })
+    btnContainer.appendChild(greyButton).classList.add('btn')
+}
+greyColor();
